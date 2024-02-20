4fc67e6ff7351d0ae92e11d40755466c0f4b1b67
Eriny Anis <124495990+ErinyAnis@users.noreply.github.com>
2024-02-20 23:33:22 +0200

    eriny

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,90 @@
+import { useState } from "react";
+import GameBoard from "./components/GameBoard";
 import Player from "./components/Player";
+import Log from "./components/Log";
+import { WINNING_COMBINATIONS } from "./components/winning-combinations";
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
+function derivedActivePlayer(gameTurns) {
+  let currentPlayer = "X";
+  if (gameTurns.length > 0 && gameTurns[0].player === "X") {
+    currentPlayer = "O";
+  }
+  return currentPlayer;
+}
 
 function App() {
+  const [gameTurns, setGameTurns] = useState([]);
+
+  const activePlayer = derivedActivePlayer(gameTurns);
+
+  let gameBoard = initialGameBoard;
+  for (const turn of gameTurns) {
+    const { square, player } = turn;
+    const { row, col } = square;
+
+    gameBoard[row][col] = player;
+  }
+
+  let winner;
+
+  for (const combination of WINNING_COMBINATIONS) {
+    const firstSquareSymbol =
+      gameBoard[combination[0].row][combination[0].column];  
+    const secondSquareSymbol =
+      gameBoard[combination[1].row][combination[1].column];   
+    const thirdSquareSymbol =
+      gameBoard[combination[2].row][combination[2].column];   
+    
+    console.log(firstSquareSymbol, secondSquareSymbol, thirdSquareSymbol);
+    
+    if (firstSquareSymbol && firstSquareSymbol === secondSquareSymbol && firstSquareSymbol === thirdSquareSymbol) {
+      winner = firstSquareSymbol;
+    }
+  }
+
+  function handleSelectSquare(rowIndex, colIndex) {
+    setGameTurns((prevTurns) => {
+      const currentPlayer = derivedActivePlayer(prevTurns);
+
+      const updatedTurns = [
+        { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
+        ...prevTurns,
+      ];
+
+      return updatedTurns;
+    });
+  }
+
   return (
     <main>
       <div id="game-container">
         {/* Players */}
-        <ol id="players">
-          <Player name="Player 1" symbol="X" />
-          <Player name="Player 2" symbol="O" />
+        <ol id="players" className="highlight-player">
+          <Player
+            initialName="Player 1"
+            symbol="X"
+            isAcative={activePlayer === "X"}
+          />
+          <Player
+            initialName="Player 2"
+            symbol="O"
+            isAcative={activePlayer === "O"}
+          />
         </ol>
-        Game Board
+
+        {winner && <p>You won,{winner} !</p>}
+
+        {/* GameBoard */}
+        <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
 
-      {/* Log */}
+      <Log turns={gameTurns} />
     </main>
   );
 }
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,30 @@
-export default function Player({name , symbol}) {
-    return (
-        <li>
-            <span className="player">
-              <span className="player-name">{name}</span>
-                <span className="player-symbol">{symbol}</span>
-            </span>
-            <button>Edit</button>
-        </li>
-    )
+import { useState } from "react";
+
+export default function Player({ initialName, symbol, isAcative }) {
+   const [playerName, setPlayerName]= useState(initialName);
+   const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing ((editing) => !editing);
+    }
+    
+    function handleChange(event) {
+        setPlayerName(event.target.value);
+    }
+
+    let editablePlayerName = <span className="player-name">{playerName}</span>;
+
+    if (isEditing) {
+        editablePlayerName = <input type="text" required value={playerName} onChange={handleChange} />;
+     }
+
+  return (
+    <li className={isAcative ? 'active' : undefined}>
+      <span className="player">
+        {editablePlayerName}    
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? 'Save': 'Edit'}</button>
+    </li>
+  );
 }
